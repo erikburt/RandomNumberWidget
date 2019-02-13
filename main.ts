092cda6d0e9913e486bafcabdf306472ad2ab8ab
erikburt <eb.erik.burton@gmail.com>
2019-02-13 15:55:52 -0500

    adding files

---
+++ main.ts
@@ -0,0 +1,35 @@
+import {app, BrowserWindow} from 'electron';
+
+let mainWindow:BrowserWindow;
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({
+    width: 200,
+    height: 200,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  });
+
+  mainWindow.loadFile('index.html')
+  mainWindow.webContents.openDevTools()
+  mainWindow.on('closed', () => { mainWindow = null; });
+}
+
+
+app.on('ready', createWindow)
+
+// Quit when all windows are closed.
+app.on('window-all-closed', () => {
+if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
